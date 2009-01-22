14760eb52bab499182a30ef9239805c5fee4c75c
Jens Kohl <jens.kohl@gmail.com>
2009-01-23 00:00:20 +0100

    Kleinen Fehler gefixt.

--- zettel9/aufgabe91_sn.c
+++ zettel9/aufgabe91_sn.c
@@ -34,9 +34,9 @@ int main(int argc, char *argv[]){
 	MPI_Init(&argc, &argv);
   	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (size != N_DIMS*M_SIZE) {
+	if (size != M_SIZE*M_SIZE) {
 		if (rank == MASTER)
-			printf("Starte bitte mit -np %d\n", N_DIMS*M_SIZE);
+			printf("Starte bitte mit -np %d\n", M_SIZE*M_SIZE);
 		exit(1);
 	}
 
