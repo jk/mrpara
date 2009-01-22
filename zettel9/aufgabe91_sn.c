fc668d1c69a5e74bf1021a2b71793f750ea349aa
Jens Kohl <jens.kohl@gmail.com>
2009-01-22 20:15:38 +0100

    Ergebnis aller Nodes an den Master schicken implementiert.

--- zettel9/aufgabe91_sn.c
+++ zettel9/aufgabe91_sn.c
@@ -34,7 +34,6 @@ int main(int argc, char *argv[]){
 	MPI_Init(&argc, &argv);
   	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
 	if (size != N_DIMS*M_SIZE) {
 		if (rank == MASTER)
 			printf("Starte bitte mit -np %d\n", N_DIMS*M_SIZE);
