162a5f939cf07dde86543ad4f428e699be1b8f85
Jens Kohl <jens.kohl@gmail.com>
2009-01-22 20:11:03 +0100

    Ergebnis aller Nodes an den Master schicken implementiert.

--- zettel9/aufgabe91_sn.c
+++ zettel9/aufgabe91_sn.c
@@ -21,10 +21,12 @@ void gentleman();
 MPI_Status status;
 MPI_Comm commCart;
 int dims[2] = {M_SIZE, M_SIZE};
-int periods[N_DIMS] = {TRUE, TRUE};
+int periods[N_DIMS] = {TRUE, TRUE}; // geht nur fuer N_DIMS=2?
 int reorder = TRUE;
 int	size, rank; 
 int	coords[N_DIMS];
+int myresult[3] = {0, 0, 0};
+int c;
 
 int *a, *b;	
 	  
@@ -32,9 +34,16 @@ int main(int argc, char *argv[]){
 	MPI_Init(&argc, &argv);
   	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (size != N_DIMS*M_SIZE) {
+		if (rank == MASTER)
+			printf("Starte bitte mit -np %d\n", N_DIMS*M_SIZE);
+		exit(1);
+	}
+
 	MPI_Cart_create(MPI_COMM_WORLD, N_DIMS, dims, periods, reorder, &commCart);
 	MPI_Cart_coords( commCart, rank, N_DIMS, coords );
-
+	
 	a = (int*)(rank*2);
 	b = (int*)(rank*2);	
 
@@ -42,17 +51,46 @@ int main(int argc, char *argv[]){
 
 	gentleman();
 	
+	if(rank == MASTER) {
+		int globalresult[M_SIZE][M_SIZE];
+		int tmpResult[3] = {0, 0, 0};
+		int i;
+		for(i = 0; i < (M_SIZE*M_SIZE)-1; i++)
+		{
+			MPI_Recv(&tmpResult, 3, MPI_INT, MPI_ANY_SOURCE, TAG, MPI_COMM_WORLD, &status);
+			//printf("%d empfaengt %d. (%d,%d) c=%d\n", rank, i+1, tmpResult[0], tmpResult[1], tmpResult[2]);
+			globalresult[tmpResult[0]][tmpResult[1]] = tmpResult[2];
+		}
+		globalresult[coords[0]][coords[1]] = c;
+		
+		printf("\nErgebnis:\n==========\n");
+		for(i = 0; i < M_SIZE; i++)
+		{
+			int j;
+			for(j = 0; j < M_SIZE; j++)
+			{
+				printf("%d ", globalresult[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	else {
+		MPI_Send(&myresult, 3, MPI_INT, MASTER, TAG, MPI_COMM_WORLD);
+		//printf("%d sendet (%d,%d) c=%d\n", rank, myresult[0], myresult[1], myresult[2]);
+	}
+	
 	MPI_Finalize();
 	return 0;
 }
 
 void vorrotation(){
-	MPI_Barrier(commCart);
+	//MPI_Barrier(commCart);
 	printf("Vor Vorrotation: Koordinaten von %d sind (%d,%d). Wert von a = %d, Wert von b = %d\n", rank, coords[0] + 1, coords[1] + 1, a, b);
 	MPI_Barrier(commCart);
 	
 	int rank_source, rank_dest;
 	
+	// Zeilenweise verschieben
 	MPI_Cart_shift(commCart, 1, DOWN*coords[0], &rank_source, &rank_dest);
 	
 	/*int coordsS[N_DIMS], coordsD[N_DIMS];
@@ -87,9 +125,11 @@ void vorrotation(){
 }
 
 void gentleman(){
-	int c = 0;
+	c = 0;
 	int i;
 	int rank_source, rank_dest;
+	myresult[0] = coords[0];
+	myresult[1] = coords[1];
 	
 	for(i = 1; i <= M_SIZE; i++){
 		c += ((int)a)*((int)b);
@@ -117,5 +157,6 @@ void gentleman(){
 		MPI_Barrier(commCart);
 	}
 	
-	printf("Ergebnis (%d,%d): %d\n", coords[0] + 1, coords[1] + 1, c);
+	myresult[2] = c;
+	//printf("Ergebnis (%d,%d): %d\n", coords[0] + 1, coords[1] + 1, c);
 }
